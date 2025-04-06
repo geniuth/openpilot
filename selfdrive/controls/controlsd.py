d926dbd88d53227657112f6002c76eb5f72bdf7f
infiniteCable2 <steffen.dahansi.werner@googlemail.com>
2025-04-06 16:59:17 +0200

    Update controlsd.py

--- selfdrive/controls/controlsd.py
+++ selfdrive/controls/controlsd.py
@@ -88,8 +88,8 @@ class Controls:
 
     steer_angle_without_offset = math.radians(CS.steeringAngleDeg - lp.angleOffsetDeg)
     self.curvature = -self.VM.calc_curvature(steer_angle_without_offset, CS.vEgo, 0)
-    self.curvature_3dof = -VM.calc_curvature_3dof(self.calibrated_pose.acceleration.y, self.calibrated_pose.acceleration.x, self.calibrated_pose.angular_velocity.yaw,
-                                                  CS.vEgo, steer_angle_without_offset, 0)
+    self.curvature_3dof = -self.VM.calc_curvature_3dof(self.calibrated_pose.acceleration.y, self.calibrated_pose.acceleration.x, self.calibrated_pose.angular_velocity.yaw,
+                                                       CS.vEgo, steer_angle_without_offset, 0)
 
     # Update Torque Params
     if self.CP.lateralTuning.which() == 'torque':
